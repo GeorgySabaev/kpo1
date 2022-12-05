284be60b955701d404ee77a5add1c18dc85add38
SaGeDeveloper <55732192+SaGeDeveloper@users.noreply.github.com>
2022-12-05 04:53:16 +0300

    menu

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,45 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Game g = new Game();
-        GameResults results = g.play();
-        if (results.isTie){
-            System.out.println("Ничья со счетом" + results.score + ".");
-        }else{
-            System.out.println("Победа игрока " + results.winner + " со счетом " + results.score + ".");
+        boolean keepRunning = true;
+        int highScore = -1;
+        String highScoreHolder = "";
+        while (keepRunning) {
+            System.out.println("РЕВЕРСИ");
+            System.out.println("Введите число от 1 до 3:");
+            System.out.println("1: Начать игру.");
+            System.out.println("2: Вывести рекорд.");
+            System.out.println("3: Выйти из игры.");
+            Scanner input = new Scanner(System.in);
+            int option = input.nextInt();
+            while (option > 3 || option < 0) {
+                System.out.println("Введите число от 1 до 3.");
+                option = input.nextInt();
+            }
+            if (option == 1) {
+                Game g = new Game();
+                GameResults results = g.play();
+                if (results.isTie) {
+                    System.out.println("Ничья со счетом" + results.score + ".");
+                } else {
+                    System.out.println("Победа игрока " + results.winner + " со счетом " + results.score + ".");
+                    if (results.isRanked && results.score > highScore){
+                        System.out.println("Новый рекорд!");
+                        highScore = results.score;
+                        highScoreHolder = results.winner;
+                    }
+                }
+            } else if (option == 2) {
+                if (highScore == -1){
+                    System.out.println("Игроки еще ни разу не победили ИИ!");
+                } else {
+                    System.out.println("Рекорд занят " + highScoreHolder + " со счетом " + highScore + ".");
+                }
+            } else {
+                System.out.println("КОНЕЦ ИГРЫ.");
+                keepRunning = false;
+            }
         }
     }
 }
